8b3d3ef215655b7155c8bbc943325c41e8fefce0
kevinbird61 <kevinbird61@gmail.com>
2018-02-28 00:19:19 +0800

    Add node.js installation scripts.

---
+++ install_nodejs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NODE_VERSION="v9.6.1"
+
+# install dependencies
+sudo apt install curl
+# using curl to download 
+curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION.tar.gz --output /tmp/node.tar.gz
+# unzip
+tar -zxvf /tmp/node.tar.gz
+
+cd /tmp/node-$NODE_VERSION
+
+# configure it
+./configure
+# build
+make -j4
+
+# test 
+make test
+# test - result
+./node -e "console.log('Hello from Node.js ' + process.version)"
+
+# install 
+sudo make install
